c70c7852bc84c23c123adba17b1b802c758d27b1
play <funblessu@gmail.com>
2018-08-10 19:44:49 +0800

    change word spell error

--- cookies_to_dict.py
+++ cookies_to_dict.py
@@ -4,7 +4,7 @@
 import Cookie
 
 
-def excute(raw_cookie):
+def execute(raw_cookie):
     cookie = Cookie.SimpleCookie()
     cookie.load(raw_cookie)
     cookies = {key: morsel.value for key, morsel in cookie.items()}
@@ -13,4 +13,4 @@ def excute(raw_cookie):
 
 if __name__ == '__main__':
     raw_cookie = "JSESSIONID=A3C196C1A0361108DD1CDC18B2080129; _gscu_1138507821=33602706z0v3a020; _gscbrs_1138507821=1; _gscu_125736681=336049870cz1v510; _gscbrs_125736681=1; Hm_lvt_9e03c161142422698f5b0d82bf699727=1533604990; Hm_lpvt_9e03c161142422698f5b0d82bf699727=1533624577; wzwsconfirm=9eba9fc3d74610ac58f7a3a8b16350c2; _gscs_1138507821=t338944902oc4m218|pv:14"
-    print(excute(raw_cookie))
+    print(execute(raw_cookie))
