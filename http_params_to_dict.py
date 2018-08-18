3648fd722508a6d1f0319c03c05e9e0cd5bf4501
hacksman <funblessu@gmail.com>
2018-08-18 10:35:31 +0800

    http_params_to_dict.py add function of filter empty

--- http_params_to_dict.py
+++ http_params_to_dict.py
@@ -3,9 +3,13 @@
 # @Time :8/17/18 15:39
 
 
-def execute(raw_params):
-    return {i.split('=')[0]: i.split('=')[1] for i in raw_params.split('&')}
+def execute(raw_params, filter_empty=True):
+    convert_params = {i.split('=')[0]: i.split('=')[1] for i in raw_params.split('&')}
+    if filter_empty:
+        convert_params = {j: k for j, k in convert_params.items() if k}
+    return convert_params
+
 
 if __name__ == '__main__':
-    raw_params = "nType=prcmNotices&pType=&prcmPrjName=&prcmItemCode=&prcmOrgName=&startDate=2018-01-01&endDate=2019-01-01&prcmPlanNo=&page=3&pageSize=18"
+    raw_params = "1=1&currentPage=1&query.title=&query.cglb=&query.gglx=3&query.cgfs=&query.time_s=&query.time_e=&query.areacode=&query.endtime_s=&query.endtime_e="
     print execute(raw_params)
